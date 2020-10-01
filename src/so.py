a74e90d8d52b67f530180d235574c4f564cfe817
zpskek <wjdghdwns0@gmail.com>
2020-10-02 06:38:01 +0900

    StackOverflow extract job

--- src/so.py
+++ src/so.py
@@ -6,11 +6,24 @@ URL = "https://stackoverflow.com/jobs?q=python"
 
 def get_last_page():
     result = requests.get(URL)
-    print(f"result:{result}")
     soup = BeautifulSoup(result.text, "html.parser")
     pages = soup.find("div", {"class": "s-pagination"}).find_all("a")
     last_page = pages[-2].get_text(strip=True)
     return int(last_page)
+
+
+def extract_job(html):
+    title = html.find("h2", {"class": "fs-body3"}).find("a")["title"]
+    company, location = html.find("h3", {"class": "fs-body1"}).find_all(
+        "span", recursive=False
+    )
+    job_id = html["data-jobid"]
+    return {
+        "title": title,
+        "company": company,
+        "location": location,
+        "link": f"https://stackoverflow.com/jobs/{job_id}",
+    }
 
 
 def extract_jobs(last_page):
@@ -20,10 +33,12 @@ def extract_jobs(last_page):
         soup = BeautifulSoup(result.text, "html.parser")
         results = soup.find_all("div", {"class": "-job"})
         for result in results:
-            print(result["data-jobid"])
+            job = extract_job(result)
+            jobs.append(job)
+    return jobs
 
 
 def get_jobs():
     last_page = get_last_page()
     jobs = extract_jobs(last_page)
-    return []
+    return jobs
