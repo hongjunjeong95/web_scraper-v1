3424678d85ad0278c113ac1c8b02315d22ba50d3
zpskek <wjdghdwns0@gmail.com>
2020-10-16 21:48:00 +0900

    Scrapper with flask

--- src/main.py
+++ src/main.py
@@ -1,8 +1,51 @@
-from indeed import get_jobs as get_indeed_jobs
-from so import get_jobs as get_so_jobs
-from save import save_to_file
+from flask import Flask, render_template, request, redirect, send_file, url_for
+from scrapper import get_jobs
+from exporter import save_to_file
 
-indeed_jobs = get_indeed_jobs()
-so_jobs = get_so_jobs()
-jobs = indeed_jobs + so_jobs
-save_to_file(jobs)
+app = Flask("SuperScrapper")
+
+db = {}
+
+# url_for("src/templates/report.html")
+
+
+@app.route("/")
+def home():
+    return render_template("user.html")
+
+
+@app.route("/report")
+def report():
+    word = request.args.get("word")
+    if word:
+        word = word.lower()
+        existingJobs = db.get(word)
+        if existingJobs:
+            jobs = existingJobs
+        else:
+            jobs = get_jobs(word)
+            db[word] = jobs
+    else:
+        return redirect("/")
+    return render_template(
+        "report.html", searchingBy=word, resultsNumber=len(jobs), jobs=jobs
+    )
+
+
+@app.route("/export")
+def export():
+    try:
+        word = request.args.get("word")
+        if not word:
+            raise Exception()
+        word = word.lower()
+        jobs = db.get(word)
+        if not jobs:
+            raise Exception()
+        save_to_file(jobs)
+        return send_file("jobs.csv")
+    except:
+        return redirect("/")
+
+
+app.run(host="127.0.0.1")
